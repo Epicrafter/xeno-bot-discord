8ffbf9b361099801cb46349f771eb14857ef3e0a
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-12-01 10:17:53 +0200

    Rule System Update

--- commands/config/setrule.js
+++ commands/config/setrule.js
@@ -10,7 +10,6 @@ module.exports = {
 
         let ruleNum = args[0];
         let rule = args.slice(1).join(' ');
-        let server = message.guild.id == '732592546068430939'; 
 
         let usage = new MessageEmbed()
         .setColor("RANDOM")
@@ -19,12 +18,6 @@ module.exports = {
 
         if(!db.get(`_mod_${message.author.id}`)) {
             usage.addField("Missing Permission", "Only ``Only Staff`` members can use this command")
-            return message.channel.send(usage)
-            .then(msg => {msg.delete({ timeout: 5000 })})
-        }
-
-        if(message.guild.id != server) {
-            usage.addField("Unauthorized Access", "Only the ``Shindo Life`` server has access to this command")
             return message.channel.send(usage)
             .then(msg => {msg.delete({ timeout: 5000 })})
         }
