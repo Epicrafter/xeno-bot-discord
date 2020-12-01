d78d3e76d88aae22881ad58543ba0c775e8956f7
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-12-01 21:36:58 +0200

    Added welcome-image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Client
 const { Client, Collection, MessageEmbed } = require('discord.js');
 const fs = require("fs");
+const Canvas = require("canvas");
 const { GiveawaysManager } = require("discord-giveaways");
 const db = require("quick.db");
 const client = new Client({
@@ -89,26 +90,67 @@ client.on('ready', s => {
 
 client.on("guildMemberAdd", (member) => {
 
-    let OnOff = db.get(`welcome_${member.guild.id}`);
+    if(member.guild.id == '376414393249824778') {
 
-    if(OnOff == 'on') {
+        let wChannel = db.get(`welchannel_${member.guild.id}`);
+        let wMessage = db.get(`welcomemsg_${member.guild.id}`);
+        let wCustomMessage = wMessage.replace(/-/, `${member.user.username}`);
 
-        let welcomechannel =  db.get(`welchannel_${member.guild.id}`);
-        let welcomemsg = db.get(`welcomemsg_${member.guild.id}`);
+        const canvas = Canvas.createCanvas(900, 389);
+        const ctx = canvas.getContext("2d");
 
-        let wcustommsg = welcomemsg.replace(/-/, `<@${member.id}>`);
+        const background = await Canvas.loadImage("https://i.imgur.com/cufdoc0.jpg");
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height); 
 
-        let embed1 = new MessageEmbed()
-        .setColor("#2980b9")
-        .setDescription(wcustommsg);
+        ctx.font = "25px sans-serif";
+        ctx.fillStyle = '#fff';
+        ctx.textAlign = "center";
+        ctx.fillText(`${wCustomMessage}`, 450, 310);
 
-        client.channels.cache.get(welcomechannel).send(embed1);
+        ctx.font = "18px sans-serif";
+        ctx.fillStyle = "#9e9e9e";
+        ctx.textAlign = "center";
+        ctx.fillText(`${member.user.tag}`, 450, 340);
+
+        ctx.arc(450, 160, 100, 0, Math.PI * 2, true)
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#fff';
+        ctx.stroke();
+        ctx.clip();
+        const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg', size: 2048, dynamic: true }));
+        ctx.drawImage(avatar, 350, 60, 200, 200);
+
+        const attachement = new MessageAttachment(
+            canvas.toBuffer(),
+            "welcome-image.png"
+        )
+
+        client.channels.cache.get(wChannel).send(attachement);
 
     } else {
-        
-        return;
 
-    }
+        let OnOff = db.get(`welcome_${member.guild.id}`);
+
+        if(OnOff == 'on') {
+
+            let welcomechannel =  db.get(`welchannel_${member.guild.id}`);
+            let welcomemsg = db.get(`welcomemsg_${member.guild.id}`);
+
+            let wcustommsg = welcomemsg.replace(/-/, `<@${member.id}>`);
+
+            let embed1 = new MessageEmbed()
+            .setColor("#2980b9")
+            .setDescription(wcustommsg);
+    
+            client.channels.cache.get(welcomechannel).send(embed1);
+
+        } else {
+            
+            return;
+
+        }
+
+    } 
 
 })
 
