862df085a6fcb45be06dff541c9b2275d4462f36
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-12-05 12:06:38 +0200

    Added events

---
+++ events/guildDelete.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Guild = require("../models/guild");
+
+module.exports = async(client, guild) => {
+
+    Guild.findOneAndDelete({
+
+        guildID: guild.id
+
+    }, (err, res) => {
+
+        if(err) console.error;
+        console.log('I have been removed from a server!');
+
+    });
+
+};
