13b81d10345e6b293b61c41e1fb44f9c084a71a3
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-12-16 20:47:00 +0200

    Update: Support.js

--- commands/info/support.js
+++ commands/info/support.js
@@ -11,7 +11,7 @@ module.exports = {
         .setColor("RANDOM")
         .setTimestamp()
         .setFooter("Powered By Xeno", client.user.avatarURL())
-        .setDescription(`To add the bot to your server [click here](https://discord.com/api/oauth2/authorize?client_id=505454012481667072&permissions=2147483639&scope=bot). To join our discord server [click here](https://discord.gg/XVyw2Jd)`)
+        .setDescription(`To add the bot to your server [click here](https://discord.com/api/oauth2/authorize?client_id=505454012481667072&permissions=2147483639&scope=applications.commands%20bot). To join our discord server [click here](https://discord.gg/XVyw2Jd)`)
 
         message.channel.send(inviteEmbed);
     }
