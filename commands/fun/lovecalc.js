738366038dc29ed6e7aaa672a1c54c19d235a548
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2020-12-17 13:16:28 +0200

    Update: lovecalc

--- commands/fun/lovecalc.js
+++ commands/fun/lovecalc.js
@@ -4,13 +4,20 @@ module.exports = {
     name: "lovecalc",
     category: "fun",
     descritpion: "Calculates the amount of Love between 2 users",
-    usage: "lovecalc [@member]",
+    usage: "lovecalc <@member> [@member]",
     run: async(client, message, args) => {
 
-        let member = message.mentions.members.first() || message.author;
+        let member = message.mentions.members.first();
+        let member2 = message.mentions.members.last();
         let love = Math.floor(Math.random() * 101);
 
-        message.channel.send(`**${message.author.username}** + ** ${member.user.username}** = ${love}% of Love ❤`)
+        console.log(member)
+
+        if(!args[1]) {
+            message.channel.send(`**${message.author.username}** + ** ${member.user.username}** = ${love}% of Love ❤`)
+        } else if(member2) {
+            message.channel.send(`**${member.user.username}** + ** ${member2.user.username}** = ${love}% of Love ❤`)
+        }
 
     }
 }
